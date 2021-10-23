2354774d923659e7b4e1a15a78e468379057e25a
hoba-android <heba96@yahoo.com>
2021-10-23 20:52:06 +0200

    Updates

---
+++ precache-manifest.5840dc71e67415c29159bcb846b9fbaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d8dfe3e9b7fad3e60c999597bc38003",
+    "url": "/hobaDevHome/colors/index.html"
+  },
+  {
+    "revision": "9f70550eece6411fd2ee",
+    "url": "/hobaDevHome/colors/static/css/main.9f3b6256.chunk.css"
+  },
+  {
+    "revision": "0b728d42613a7c9ec6d3",
+    "url": "/hobaDevHome/colors/static/js/2.cdd6e70d.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/hobaDevHome/colors/static/js/2.cdd6e70d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f70550eece6411fd2ee",
+    "url": "/hobaDevHome/colors/static/js/main.590ce5c3.chunk.js"
+  },
+  {
+    "revision": "a923325cb4ed2c349cdf",
+    "url": "/hobaDevHome/colors/static/js/runtime-main.bc3c68a5.js"
+  }
+]);
